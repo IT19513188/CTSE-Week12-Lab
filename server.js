09e420d6bf4f0ecc6896a770631ead75f0c41b9d
IT19513188 <it19513188@my.sliit.lk>
2022-05-14 16:03:27 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/",(req,res)=>{
+    res.send("Hi, Week 12 Lab - IT19513188");
+})
+
+app.listen(PORT , () => {
+    console.log(`Server is running on ${PORT}`);
+});
